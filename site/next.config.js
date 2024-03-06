35fde7a0d773e6d94b79f5d0f75960f27ae60dbf
dooshub <162514331+dooshub@users.noreply.github.com>
2024-03-06 23:52:06 +0100

    Update next.config.js

--- site/next.config.js
+++ site/next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   env: {
     name: 'dOOs',
     description: 'The official store for dOOs supported Kasm workspaces.',
-    icon: 'https://dooshub.github.io/kasm-registry/1.0/img/logo.png',
+    icon: 'https://dooshub.github.io/kasm-registry/1.0/logo.png',
     listUrl: 'https://dooshub.github.io/kasm-registry/',
     contactUrl: 'https://github.com/dooshub/kasm-registry/issues',
   },
